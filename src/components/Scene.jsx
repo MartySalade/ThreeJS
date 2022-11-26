3631d854bcafa4850262a3aba8748ff46d68fc48
MartySalade <malleinmartin@gmail.com>
2022-11-26 15:51:22 +0100

    Close button for dezooming

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -6,11 +6,36 @@ export default class Scene extends React.Component {
   constructor(props) {
     super(props);
     this.canvasRef = React.createRef();
+    this.camera_position = new THREE.Vector3(50, 50, 50);
   }
 
   componentDidMount() {
+    // Triggered when an element is clicked on the scene
+    const onDocumentMouseDown = (event) => {
+      event.preventDefault();
+      mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
+      mouse.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1;
+      raycaster.setFromCamera(mouse, camera);
+
+      var intersects = raycaster.intersectObjects(scene.children);
+
+      if (intersects.length > 0) {
+        // Call a specific function foreach building or citizen
+        intersects[0].object.callback();
+
+        // Set the new position of the camera depeding on the object
+        this.camera_position.copy(
+          new THREE.Vector3(
+            intersects[0].object.position.x + 20,
+            intersects[0].object.position.y + 20,
+            intersects[0].object.position.z + 20
+          )
+        );
+      }
+    };
+
     var scene = new THREE.Scene();
-    scene.background = new THREE.Color("#FF0000");
+    scene.background = new THREE.Color("#130f40");
     var camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
@@ -26,19 +51,18 @@ export default class Scene extends React.Component {
 
     // ========== Mesh Creation ========== //
     var geometry = new THREE.BoxGeometry(10, 20, 10);
-    var material = new THREE.MeshBasicMaterial({ color: 0x130f40 });
+    var material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
     var cube = new THREE.Mesh(geometry, material);
 
     var geometry3 = new THREE.BoxGeometry(15, 30, 15);
     var material2 = new THREE.MeshBasicMaterial({ color: 0x0000ff });
     var cube2 = new THREE.Mesh(geometry3, material2);
-    var camera_position = new THREE.Vector3(50, 50, 50);
 
     cube.callback = function () {
-      console.log("Building1");
+      // console.log("Building1");
     };
     cube2.callback = function () {
-      console.log("Building2");
+      // console.log("Building2");
     };
 
     cube.position.set(0, 10, 0);
@@ -59,45 +83,30 @@ export default class Scene extends React.Component {
     var mouse = new THREE.Vector2();
     window.addEventListener("click", onDocumentMouseDown, false);
 
-    // Triggered when an element is clicked on the scene
-    function onDocumentMouseDown(event) {
-      event.preventDefault();
-      mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
-      mouse.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1;
-      raycaster.setFromCamera(mouse, camera);
-
-      var intersects = raycaster.intersectObjects(scene.children);
-
-      if (intersects.length > 0) {
-        // Call a specific function foreach building or citizen
-        intersects[0].object.callback();
-
-        // Set the new position of the camera depeding on the object
-        camera_position.copy(
-          new THREE.Vector3(
-            intersects[0].object.position.x + 20,
-            intersects[0].object.position.y + 20,
-            intersects[0].object.position.z + 20
-          )
-        );
-      }
-    }
-
     // Main loop
-    var animate = function () {
+    var animate = () => {
       requestAnimationFrame(animate);
       controls.update();
       // Smooth move for the camera
-      camera.position.lerp(camera_position, 0.05);
+      camera.position.lerp(this.camera_position, 0.05);
       renderer.render(scene, camera);
     };
 
+    //Reset camera
+
     animate();
   }
-
+  resetCamera = () => {
+    this.camera_position.copy(new THREE.Vector3(50, 50, 50));
+  };
   componentWillUnmount() {}
-
   render() {
-    return <div />;
+    return (
+      <div>
+        <button className="button_close" onClick={this.resetCamera}>
+          X
+        </button>
+      </div>
+    );
   }
 }
